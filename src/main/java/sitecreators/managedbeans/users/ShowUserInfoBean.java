ebd97ffc1d1428bb737a3e77fd3b5b8f245ca584
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-01 16:36:25 +0300

    added user home page with bean and composite

--- src/main/java/sitecreators/managedbeans/users/ShowUserInfoBean.java
+++ src/main/java/sitecreators/managedbeans/users/ShowUserInfoBean.java
@@ -7,10 +7,12 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 import sitecreators.utils.ApplicationContextUtil;
 import sitecreators.utils.comment.Comment;
 import sitecreators.utils.image.Image;
+import sitecreators.utils.image.ImageDAO;
 import sitecreators.utils.user.User;
 import sitecreators.utils.user.UserAbout;
 import sitecreators.utils.user.UserContacts;
@@ -19,14 +21,23 @@ import sitecreators.utils.user.UserDAO;
 @ManagedBean(name="showUserInfoBean")
 public class ShowUserInfoBean {
 	
-	@ManagedProperty(value="#{param.ownerId}")
-	private String ownerId;
+	private User visitor;
 	
-	private User owner;
+	private String id;
 	
-	private UserAbout about;
+	private ImageDAO imageDao;
 	
-	private UserContacts contacts;
+	private String firstName;
+	
+	private String secondName;
+	
+	private String userAbout;
+	
+	private String pnoneNumber;
+
+	private String email;
+
+	private String newComment;
 	
 	private Image icon;
 	
@@ -40,47 +51,97 @@ public class ShowUserInfoBean {
 	
 	public ShowUserInfoBean(){
 		this.userDao = (UserDAO) ApplicationContextUtil.getApplicationContext().getBean("UserDAO");
-		long userId =(long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
-		this.user = userDao.getUser(userId);
-		this.owner = userDao.getUser(Long.parseLong(ownerId));
-		this.about = owner.getAbout();
-		this.contacts = owner.getContacts();
-		this.icon = owner.getIcon();
-		this.images = owner.getImages();
-		this.comments = owner.getComments();
+		long visitorId = 1;//(long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
+		HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		this.id = (String) req.getParameter("userId");
+		long userId = Long.parseLong(this.id);
+		try{
+			userDao.open();
+			this.visitor = userDao.getUser(visitorId);
+			this.user = userDao.getUser(userId);
+			UserAbout uAbout = user.getAbout();
+			if(uAbout != null){
+				this.firstName = uAbout.getFirstName();
+				this.secondName = uAbout.getSecondName();
+				this.userAbout = uAbout.getAboutInfo();
+			}
+			UserContacts uContact = user.getContacts();
+			if(uContact != null){
+				this.pnoneNumber = uContact.getPnoneNumber();
+				this.email = uContact.getEmail();
+			}
+			this.icon = user.getIcon();
+			this.images = user.getImages();
+			this.comments = user.getComments();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 	
-	public void addComment(String body){
+	public void closeSession(){
+		userDao.close();
+	}
+	public void addComment(){
 		Comment comment = new Comment();
-		comment.setBody(body);
+		comment.setBody(newComment);
 		comment.setPublisher(user);
 		comment.setPublishTime(new Timestamp(new Date().getTime()));
-		owner.addComment(comment);
-		userDao.updateUser(owner);
+		try{
+			userDao.open();
+			user.addComment(comment);
+			userDao.updateUser(user);
+			userDao.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
-	public String getOwnerId() {
-		return ownerId;
+	public String getId() {
+		return id;
 	}
 
-	public void setOwnerId(String ownerId) {
-		this.ownerId = ownerId;
+	public void setId(String id) {
+		this.id = id;
 	}
 
-	public UserAbout getAbout() {
-		return about;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setAbout(UserAbout about) {
-		this.about = about;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
-	public UserContacts getContacts() {
-		return contacts;
+	public String getSecondName() {
+		return secondName;
 	}
 
-	public void setContacts(UserContacts contacts) {
-		this.contacts = contacts;
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getPnoneNumber() {
+		return pnoneNumber;
+	}
+
+	public void setPnoneNumber(String pnoneNumber) {
+		this.pnoneNumber = pnoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNewComment() {
+		return newComment;
+	}
+
+	public void setNewComment(String newComment) {
+		this.newComment = newComment;
 	}
 
 	public Image getIcon() {
@@ -107,4 +168,12 @@ public class ShowUserInfoBean {
 		this.comments = comments;
 	}
 
+	public String getUserAbout() {
+		return userAbout;
+	}
+
+	public void setUserAbout(String userAbout) {
+		this.userAbout = userAbout;
+	}
+	
 }
