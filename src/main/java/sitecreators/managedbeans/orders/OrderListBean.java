f8c0634352abdde1a8189acbda7a63a8d35663e6
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-02 12:50:27 +0300

    added orders page with composite and bean

--- src/main/java/sitecreators/managedbeans/orders/OrderListBean.java
+++ src/main/java/sitecreators/managedbeans/orders/OrderListBean.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 import sitecreators.utils.ApplicationContextUtil;
 import sitecreators.utils.order.Order;
 import sitecreators.utils.order.OrderDAO;
+import sitecreators.utils.product.Product;
+import sitecreators.utils.product.ProductDAO;
 import sitecreators.utils.user.User;
 import sitecreators.utils.user.UserDAO;
 
@@ -16,17 +19,52 @@ public class OrderListBean {
 	
 	private OrderDAO orderDao;
 	
+	private UserDAO userDao;
+	
+	private ProductDAO productDao;
+	
 	private List<Order> orders;
+	
+	private Product product = null;
+	
+	private User user, customer = null;
 	
 	
 	public OrderListBean(){
-		orderDao = (OrderDAO) ApplicationContextUtil.getApplicationContext().getBean("CategoryDAO");
-		orders = orderDao.getOrders();
-		long userId =(long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
-		UserDAO userDao = (UserDAO) ApplicationContextUtil.getApplicationContext().getBean("UserDAO");
-		User user = userDao.getUser(userId);
+		userDao = (UserDAO) ApplicationContextUtil.getApplicationContext().getBean("UserDAO");
+		orderDao = (OrderDAO) ApplicationContextUtil.getApplicationContext().getBean("OrderDAO");
+		productDao = (ProductDAO) ApplicationContextUtil.getApplicationContext().getBean("ProductDAO");
+		long userId = 1;//(long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
+		HttpServletRequest req = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		String productId = (String) req.getParameter("productId");
+		String customerId = (String) req.getParameter("customerId");
+		try{
+		if(productId != null){
+			productDao.open();
+			this.product = productDao.getProduct(Long.parseLong(productId));
+			orders = product.getOrders();
+		} else if(customerId != null){
+			userDao.open();
+			customer = userDao.getUser(Long.parseLong(customerId));
+			orders = customer.getPurchases();
+		} else {
+			userDao.open();
+			user = userDao.getUser(userId);
+			if(userId == 1){
+				orders = orderDao.getOrders();
+			} else orders = user.getPurchases();
+		}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
-
+	
+	public void closeSession(){
+		productDao.close();
+		userDao.close();
+	}
+	
+	
 	public List<Order> getOrders() {
 		return orders;
 	}
