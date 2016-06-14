1d55fb3ba16d2c8455bc2579aa9fb99615645941
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-14 16:47:48 +0300

    changed login.xhtml for LoginBean logic

---
+++ src/main/java/sitecreators/managedbeans/bootshop/LoginBean.java
@@ -0,0 +1,145 @@
+package sitecreators.managedbeans.bootshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import sitecreators.utils.ApplicationContextUtil;
+import sitecreators.utils.auth.Password;
+import sitecreators.utils.category.Category;
+import sitecreators.utils.category.CategoryDAO;
+import sitecreators.utils.order.Order;
+import sitecreators.utils.order.OrderStatus;
+import sitecreators.utils.user.User;
+import sitecreators.utils.user.UserAbout;
+import sitecreators.utils.user.UserDAO;
+
+@ManagedBean(name="loginBean")
+public class LoginBean {
+	
+	private User user;
+
+	private UserDAO userDao; 
+
+	private long userId = 0;
+	
+	private String userName;
+	
+	private List<Order> cart = new ArrayList<>();
+	
+	private CategoryDAO categoryDao;
+	
+	private List<Category> categories;
+	
+	private String loginEmail;
+	
+	private String loginPassword;
+	
+	public LoginBean(){
+		this.userDao = (UserDAO) ApplicationContextUtil.getApplicationContext().getBean("UserDAO");
+		this.categoryDao =(CategoryDAO) ApplicationContextUtil.getApplicationContext().getBean("CategoryDAO");
+		userId = (long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
+		try{
+			userDao.open();
+			this.user = userDao.getUser(userId);
+			UserAbout uAbout = user.getAbout();
+			if(uAbout != null){
+				this.userName = uAbout.getFirstName()+" "+uAbout.getSecondName();
+			}
+			List<Order> orders = user.getPurchases();
+			for(Order o : orders){
+				if(o.getStatus().equals(OrderStatus.INCART)) this.cart.add(o);
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		try{
+			categoryDao.open();
+			this.categories = categoryDao.getAllCategories();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String login(){
+		try{
+			userDao.open();
+			this.user = userDao.getUser(loginEmail);
+			Password pswd = user.getPassword();
+			if(pswd.check(loginPassword)){
+				FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userID", user.getId());
+				System.out.println("password accepted");
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			userDao.close();
+		}
+		
+		return "products";
+	}
+	
+	public void closeSession(){
+		userDao.close();
+		categoryDao.close();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public List<Order> getCart() {
+		return cart;
+	}
+
+	public void setCart(List<Order> cart) {
+		this.cart = cart;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public String getLoginEmail() {
+		return loginEmail;
+	}
+
+	public void setLoginEmail(String loginEmail) {
+		this.loginEmail = loginEmail;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+		
+}
