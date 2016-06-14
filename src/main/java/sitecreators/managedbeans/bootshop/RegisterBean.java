f31c9b0b455e3e0f9b7e12f366c04e6c3e381492
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-14 16:27:49 +0300

    added register.xhtml with managed bean

---
+++ src/main/java/sitecreators/managedbeans/bootshop/RegisterBean.java
@@ -0,0 +1,224 @@
+package sitecreators.managedbeans.bootshop;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import sitecreators.utils.ApplicationContextUtil;
+import sitecreators.utils.auth.Password;
+import sitecreators.utils.category.Category;
+import sitecreators.utils.category.CategoryDAO;
+import sitecreators.utils.image.Image;
+import sitecreators.utils.image.ImageDAO;
+import sitecreators.utils.order.Order;
+import sitecreators.utils.order.OrderStatus;
+import sitecreators.utils.user.User;
+import sitecreators.utils.user.UserAbout;
+import sitecreators.utils.user.UserContacts;
+import sitecreators.utils.user.UserDAO;
+
+@ManagedBean(name="registerBean")
+public class RegisterBean {
+	
+	private User user;
+
+	private UserDAO userDao; 
+
+	private long userId = 0;
+	
+	private String userName;
+	
+	private List<Order> cart = new ArrayList<>();
+	
+	private CategoryDAO categoryDao;
+	
+	private List<Category> categories;
+	
+	private String loginEmail;
+	
+	private String loginPassword;
+	
+	private String firstName;
+	
+	private String secondName;
+	
+	private String email;
+	
+	private String password;
+	
+		
+	public RegisterBean(){
+		this.userDao = (UserDAO) ApplicationContextUtil.getApplicationContext().getBean("UserDAO");
+		this.categoryDao =(CategoryDAO) ApplicationContextUtil.getApplicationContext().getBean("CategoryDAO");
+		userId = (long) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userID");
+		try{
+			userDao.open();
+			this.user = userDao.getUser(userId);
+			UserAbout uAbout = user.getAbout();
+			if(uAbout != null){
+				this.userName = uAbout.getFirstName()+" "+uAbout.getSecondName();
+			}
+			List<Order> orders = user.getPurchases();
+			for(Order o : orders){
+				if(o.getStatus().equals(OrderStatus.INCART)) this.cart.add(o);
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		try{
+			categoryDao.open();
+			this.categories = categoryDao.getAllCategories();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String register(){
+		User user = new User();
+		UserAbout about = new UserAbout();
+		about.setFirstName(firstName);
+		about.setSecondName(secondName);
+		user.setAbout(about);
+		UserContacts contacts = new UserContacts();
+		contacts.setEmail(email);
+		user.setContacts(contacts);
+		Password pass = new Password();
+		try {
+			pass.create(this.password);
+		} catch (NoSuchAlgorithmException e) {
+			return "internalError";
+		}
+		user.setPassword(pass);
+		ImageDAO imageDao = (ImageDAO) ApplicationContextUtil.getApplicationContext().getBean("ImageDAO");
+		Image icon = new Image();
+		String filePath = imageDao.saveImage(null, "users", 0);
+		icon.setImagePath(filePath);
+		icon.setImgDecs("no image");
+		imageDao.addImage(icon);
+		user.setIcon(icon);
+		try{
+			userDao.open();
+			userDao.addUser(user);
+		} catch (Exception e){
+			e.printStackTrace();
+			return "internalError";
+		} finally {
+			userDao.close();
+		}
+		
+		return "home";
+	}
+	
+	public void login(){
+		try{
+			userDao.open();
+			this.user = userDao.getUser(loginEmail);
+			Password pswd = user.getPassword();
+			if(pswd.check(loginPassword)){
+				FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userID", user.getId());
+				System.out.println("password accepted");
+			}
+		} catch (Exception e){
+			e.printStackTrace();
+		} finally {
+			userDao.close();
+		}
+	}
+	
+	public void closeSession(){
+		userDao.close();
+		categoryDao.close();
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public List<Order> getCart() {
+		return cart;
+	}
+
+	public void setCart(List<Order> cart) {
+		this.cart = cart;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+
+	public String getLoginEmail() {
+		return loginEmail;
+	}
+
+	public void setLoginEmail(String loginEmail) {
+		this.loginEmail = loginEmail;
+	}
+
+	public String getLoginPassword() {
+		return loginPassword;
+	}
+
+	public void setLoginPassword(String loginPassword) {
+		this.loginPassword = loginPassword;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+		
+}
