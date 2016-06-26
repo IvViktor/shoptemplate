ffa9ad48732fae4087d69626fa3515e545d818b7
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-26 09:54:39 +0300

    added functionality to country class

--- src/main/java/sitecreators/utils/finance/Country.java
+++ src/main/java/sitecreators/utils/finance/Country.java
@@ -1,5 +1,28 @@
 package sitecreators.utils.finance;
 
 public enum Country {
-	UAH, USD, EUR, RUR, GBP;
+	UAH("UAH",'\u20b4'),
+	USD("USD",'$'),
+	EUR("EUR",'\u20ac'),
+	RUB("RUB",'\u20bd'),
+	GBP("GBP",'\u20a4');
+	
+	private final String stringValue;
+	
+	private final char cc;
+	
+	private Country(String stringValue, char cc) {
+		this.stringValue = stringValue;
+		this.cc = cc;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public char getCc() {
+		return cc;
+	}
+	
+	
 }
