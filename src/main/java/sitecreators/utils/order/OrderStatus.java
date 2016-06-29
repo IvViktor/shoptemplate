2eb4ae7ab2c28b2918ed1a88b8df7b71acd29707
Viktor Ivanchenko <viittyok1992@gmail.com>
2016-06-29 18:09:03 +0300

    modified orderstatus enum

--- src/main/java/sitecreators/utils/order/OrderStatus.java
+++ src/main/java/sitecreators/utils/order/OrderStatus.java
@@ -2,6 +2,21 @@ package sitecreators.utils.order;
 
 public enum OrderStatus {
 	
-	ACTIVE, FULLFILLED,DEFFERED,CANCELED,INCART;
+	ACTIVE("ACTIVE"),
+	FULLFILLED("FULLFILLED"),
+	DEFFERED("DEFFERED"),
+	CANCELED("CANCELED"),
+	INCART("INCART");
 	
+	private final String representValue;
+	
+	private OrderStatus(String str) {
+		this.representValue = str;
+	}
+
+	public String getRepresentValue() {
+		return representValue;
+	}
+
+		
 }
